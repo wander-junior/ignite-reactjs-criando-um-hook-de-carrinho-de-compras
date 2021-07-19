bd9433e4a0f11d4de33e48e7323515edecd63ed3
Wander Júnior Cruz <wanderjrcruz96@gmail.com>
2021-07-19 10:58:41 -0300

    Implement cartItemsAmount

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -23,11 +23,12 @@ interface CartItemsAmount {
 
 const Home = (): JSX.Element => {
   const [products, setProducts] = useState<ProductFormatted[]>([]);
-  // const { addProduct, cart } = useCart();
+  const { addProduct, cart } = useCart();
 
-  // const cartItemsAmount = cart.reduce((sumAmount, product) => {
-  //   // TODO
-  // }, {} as CartItemsAmount)
+  const cartItemsAmount = cart.reduce((sumAmount, product) => {
+    sumAmount[product.id] = product.amount;
+    return sumAmount;
+  }, {} as CartItemsAmount)
 
   useEffect(() => {
     async function loadProducts() {
@@ -46,7 +47,7 @@ const Home = (): JSX.Element => {
     <ProductList>
       {products.map(product => 
         <li key={product.id}>
-          <img src={product.image} title={product.title}/>
+          <img src={product.image} alt={product.title}/>
           <strong>{product.title}</strong>
           <span>R$ {product.price}</span>
           <button
@@ -56,7 +57,7 @@ const Home = (): JSX.Element => {
           >
             <div data-testid="cart-product-quantity">
               <MdAddShoppingCart size={16} color="#FFF" />
-              {/* {cartItemsAmount[product.id] || 0} */} 0
+              {cartItemsAmount[product.id] || 0}
             </div>
             <span>ADICIONAR AO CARRINHO</span>
           </button>
